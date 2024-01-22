9d8b45837a2560cd2df8d9de744af40595c543db
mpierangeli <mpierangeli@estudiantes.unsam.edu.ar>
2024-01-22 00:56:20 -0300

    fixing packaging

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from .predictor import model_downloader, load_example, model_loading, predict, seg_plot
+from predictor import model_downloader, load_example, model_loading, predict, seg_plot
 
 
 __all__ = ['model_downloader', 'load_example', 'model_loading', 'predict', 'seg_plot']
