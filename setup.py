aa77c8df042088a4adc45425592da7c5544b2aac
mpierangeli <mpierangeli@estudiantes.unsam.edu.ar>
2024-01-22 12:30:29 -0300

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='psmoe',
-    version='0.1.4.1',
+    version='0.1.4.2',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.23.5',
